a7bb6f0f969f4141c7e734a1f688c28dff0199b6
mohemohe <mohemohe@ghippos.net>
2019-06-28 02:10:38 +0900

    WIP User

--- client/admin/containers/page/User.tsx
+++ client/admin/containers/page/User.tsx
@@ -1,14 +1,22 @@
 import * as React from "react";
+import {inject} from "mobx-react";
+import {UserStore} from "../../stores/UserStore";
 
 interface IProps extends React.ClassAttributes<{}> {
+    UserStore?: UserStore;
 }
 
 interface IState extends React.ComponentState {
 }
 
+@inject("UserStore")
 export class User extends React.Component<IProps, IState> {
     constructor(props: IProps, state: IState) {
         super(props, state);
+    }
+
+    public componentDidMount() {
+        this.props.UserStore!.getUsers(0);
     }
 
     public render() {
--- client/admin/stores/index.ts
+++ client/admin/stores/index.ts
@@ -1,11 +1,13 @@
 import {RouterStore} from "mobx-react-router";
 import ToastStore from "../../common/stores/ToastStore";
 import {AuthStore} from "./AuthStore";
+import {UserStore} from "./UserStore";
 
 const stores = {
     AuthStore: new AuthStore(),
     ToastStore: new ToastStore(),
     RouterStore: new RouterStore(),
+    UserStore: new UserStore(),
 };
 
 export default stores;
