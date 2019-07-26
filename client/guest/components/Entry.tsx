df3157767cd093a6740cbde5a789c37602f9bedd
mohemohe <mohemohe@ghippos.net>
2019-07-27 03:25:56 +0900

    close #25

--- client/guest/components/Entry.tsx
+++ client/guest/components/Entry.tsx
@@ -3,7 +3,6 @@ import {style} from "typestyle";
 import {IEntry} from "../stores/EntryStore";
 import {COLORS} from "../constants/Style";
 import Card, {CardProps} from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
 import ReactMarkdown from "react-markdown";
 import {Link} from "react-router-dom";
 
@@ -23,6 +22,7 @@ const styles = {
         background: COLORS.BaseColor,
         color: `${COLORS.EmotionalWhite} !important`,
         borderRadius: "4px 4px 0 0",
+        padding: "1rem",
         $nest: {
             "& a": {
                 color: `${COLORS.EmotionalWhite} !important`,
@@ -33,9 +33,13 @@ const styles = {
             },
         },
     }),
+    link: style({
+
+    }),
     subheader: style({
         display: "flex",
         flexWrap: "wrap",
+        opacity: 0.56,
         $nest: {
             "& > *": {
                 marginRight: "1em",
@@ -55,15 +59,15 @@ export class Entry extends React.Component<IProps, {}> {
 
         return (
             <Card {...this.props} className={styles.root} elevation={6}>
-                <CardHeader
-                    className={styles.title}
-                    title={<Link to={`/entry/${entry._id}`}>{entry.title}</Link>}
-                    subheader={
-                        <div className={styles.subheader}>
-                            <div>公開: {created.toLocaleString()},</div>
-                            <div>更新: {modified.toLocaleString()}</div>
-                        </div>
-                    }/>
+                <div className={styles.title}>
+                    <h2 className={styles.link}>
+                        <Link to={`/entry/${entry._id}`}>{entry.title}</Link>
+                    </h2>
+                    <div className={styles.subheader}>
+                        <div>公開: {created.toLocaleString()},</div>
+                        <div>更新: {modified.toLocaleString()}</div>
+                    </div>
+                </div>
                 <ReactMarkdown source={entry.body} className={`markdown-body ${styles.body}`} plugins={[[breaks]]} escapeHtml={false}/>
             </Card>
         )
