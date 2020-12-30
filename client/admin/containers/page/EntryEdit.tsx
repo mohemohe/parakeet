aa0bb06113c56d7b48d9033125eb216cdc4b8015
mohemohe <mohemohe@ghippos.net>
2020-12-30 18:32:39 +0900

    close #103

--- client/admin/containers/page/EntryEdit.tsx
+++ client/admin/containers/page/EntryEdit.tsx
@@ -5,7 +5,7 @@ import {inject, observer} from "mobx-react";
 import {EntryStore, IEntry} from "../../stores/EntryStore";
 import {RouteComponentProps} from "react-router";
 import {style} from "typestyle";
-import {Fab, FormControl, FormControlLabel, Switch} from "@material-ui/core";
+import {Box, Fab, FormControl, FormControlLabel, Switch} from "@material-ui/core";
 import Save from "@material-ui/icons/Save";
 import {ValidatableTextField} from "../../components/ValidatableTextField";
 import {TitleBar} from "../../../common/components/TitleBar";
@@ -22,11 +22,23 @@ const styles = {
         display: "block",
     }),
     simpleMDE: style({
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+
         $nest: {
             "& .fullscreen": {
                 zIndex: 1000,
             },
+            "& .CodeMirror": {
+                height: "300px !important",
+                flex: 1,
+            },
+            "& .editor-statusbar": {
+                marginRight: 64,
+            },
             "& .CodeMirror-fullscreen": {
+                height: "auto !important",
                 zIndex: 1000,
             },
             "& .CodeMirror-fullscreen + .editor-preview": {
@@ -84,49 +96,58 @@ export class EntryEdit extends React.Component<IProps, IState> {
         };
 
         return (
-            <div>
+            <>
                 <TitleBar>{pageTitle}</TitleBar>
-                <FormControl className={styles.control}>
-                    <ValidatableTextField
-                        label={"タイトル"}
-                        fullWidth={true}
-                        validators={[]}
-                        onChangeValue={(event) => this.props.EntryStore!.setEntry({...entry, title: event.target.value})}
-                        value={title}
-                        InputLabelProps={{shrink: true}}
-                    />
-                </FormControl>
-                <FormControlLabel
-                    className={styles.control}
-                    control={
-                        <Switch
-                            checked={draft}
-                            onChange={(event) => this.props.EntryStore!.setEntry({...entry, draft: event.target.checked})}
-                            color="primary"
+                <Box>
+                    <FormControl className={styles.control}>
+                        <ValidatableTextField
+                            label={"タイトル"}
+                            fullWidth={true}
+                            validators={[]}
+                            onChangeValue={(event) => this.props.EntryStore!.setEntry({...entry, title: event.target.value})}
+                            value={title}
+                            InputLabelProps={{shrink: true}}
                         />
-                    }
-                    label="下書き"
-                />
-                <SimpleMDE
-                    className={styles.simpleMDE}
-                    onChange={(body) => this.props.EntryStore!.setEntry({...entry, body})}
-                    value={body}
-                    options={{
-                        spellChecker: false,
-                        previewClass: ["editor-preview", "markdown-body"],
-                        toolbar: [
-                            "bold", "italic", "strikethrough", "|",
-                            "heading-smaller", "heading-bigger", "|",
-                            "code", "quote", "unordered-list", "ordered-list", "table", "|",
-                            "link", "image", insertReadMore, "|",
-                            "preview", "side-by-side", "fullscreen", "|",
-                            "guide",
-                        ],
-                    }}/>
+                    </FormControl>
+                </Box>
+
+                <Box>
+                    <FormControlLabel
+                        className={styles.control}
+                        control={
+                            <Switch
+                                checked={draft}
+                                onChange={(event) => this.props.EntryStore!.setEntry({...entry, draft: event.target.checked})}
+                                color="primary"
+                            />
+                        }
+                        label="下書き"
+                    />
+                </Box>
+
+                <Box display={"flex"} flex={1} width={"100%"}>
+                    <SimpleMDE
+                        className={styles.simpleMDE}
+                        onChange={(body) => this.props.EntryStore!.setEntry({...entry, body})}
+                        value={body}
+                        options={{
+                            spellChecker: false,
+                            previewClass: ["editor-preview", "markdown-body"],
+                            toolbar: [
+                                "bold", "italic", "strikethrough", "|",
+                                "heading-smaller", "heading-bigger", "|",
+                                "code", "quote", "unordered-list", "ordered-list", "table", "|",
+                                "link", "image", insertReadMore, "|",
+                                "preview", "side-by-side", "fullscreen", "|",
+                                "guide",
+                            ],
+                        }}/>
+                </Box>
+
                 <Fab className={styles.saveButton} color={"primary"} onClick={() => this.props.EntryStore!.putEntry()}>
                     <Save/>
                 </Fab>
-            </div>
+            </>
         );
     }
 }
